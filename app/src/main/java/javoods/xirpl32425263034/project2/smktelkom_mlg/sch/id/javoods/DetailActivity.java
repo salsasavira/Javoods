3e3153f6e3a7be56499d73c44228ae859a064ef1
nurwid28 <nur_astutik_24rpl@student.smktelkom-mlg.sch.id>
2017-04-01 07:30:12 +0700

    button detail 1

---
+++ app/src/main/java/javoods/xirpl32425263034/project2/smktelkom_mlg/sch/id/javoods/DetailActivity.java
@@ -0,0 +1,13 @@
+package javoods.xirpl32425263034.project2.smktelkom_mlg.sch.id.javoods;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
--- app/src/main/java/javoods/xirpl32425263034/project2/smktelkom_mlg/sch/id/javoods/Fragment/CemilanFragment.java
+++ app/src/main/java/javoods/xirpl32425263034/project2/smktelkom_mlg/sch/id/javoods/Fragment/CemilanFragment.java
@@ -1,11 +1,16 @@
 package javoods.xirpl32425263034.project2.smktelkom_mlg.sch.id.javoods.Fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import javoods.xirpl32425263034.project2.smktelkom_mlg.sch.id.javoods.DetailActivity;
 import javoods.xirpl32425263034.project2.smktelkom_mlg.sch.id.javoods.R;
 
 
@@ -14,6 +19,7 @@ import javoods.xirpl32425263034.project2.smktelkom_mlg.sch.id.javoods.R;
  */
 public class CemilanFragment extends Fragment {
 
+    ImageButton imLihat;
 
     public CemilanFragment() {
         // Required empty public constructor
@@ -23,8 +29,34 @@ public class CemilanFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View v =  inflater.inflate(R.layout.fragment_cemilan, container, false);
+
+
+        imLihat = (ImageButton) getView().findViewById(R.id.ImagebuttonLihatCemilan);
+
+        imLihat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), DetailActivity.class));
+            }
+
+    });
+        return v;
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cemilan, container, false);
+        //return inflater.inflate(R.layout.fragment_cemilan, container, false);
+        //Intent i = new Intent(getActivity(), DetailActivity.class);startActivity(i);
+        //View view = inflater.inflate(R.layout.fragment_cemilan,container,false);
+        //ImageButton btnaye = (ImageButton) getView().findViewById(R.id.ImagebuttonLihatCemilan);
+        //btnaye.setOnClickListener(new View.OnClickListener() {
+          //  @Override
+          //  public void onClick(View v) {
+           //       startActivity(new Intent(getActivity(), DetailActivity.class));
+
+          //  }
+       // });
+      //  return view;
     }
 
 }
