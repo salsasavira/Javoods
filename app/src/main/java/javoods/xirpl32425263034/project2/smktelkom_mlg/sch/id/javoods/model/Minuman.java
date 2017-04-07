96032382cd1d4b451ffea895233bc368d7eebdcb
nadiaputrinur <nadiaputri395@gmail.com>
2017-04-07 13:46:52 +0700

    input Minuman revisi

--- app/src/main/java/javoods/xirpl32425263034/project2/smktelkom_mlg/sch/id/javoods/model/Minuman.java
+++ app/src/main/java/javoods/xirpl32425263034/project2/smktelkom_mlg/sch/id/javoods/model/Minuman.java
@@ -1,20 +1,32 @@
 package javoods.xirpl32425263034.project2.smktelkom_mlg.sch.id.javoods.model;
 
+import com.orm.SugarRecord;
+
+import java.io.Serializable;
+
 /**
  * Created by User on 31/03/2017.
  */
 
-public class Minuman {
+public class Minuman extends SugarRecord implements Serializable {
+
     public String judulminuman;
     public String deskripsiminuman;
     public String detailminuman;
     public String fotominuman;
 
+    public Minuman() {
+    }
 
-    public Minuman(String judul, String deskripsi, String detail, String foto) {
-        this.judulminuman = judul;
-        this.deskripsiminuman = deskripsi;
-        this.detailminuman = detail;
-        this.fotominuman = foto;
+    public Minuman(String judulminuman, String deskripsiminuman, String detailminuman, String fotominuman) {
+        this.judulminuman = judulminuman;
+        this.deskripsiminuman = deskripsiminuman;
+        this.detailminuman = detailminuman;
+        this.fotominuman = fotominuman;
+    }
+
+    @Override
+    public String toString() {
+        return judulminuman + "\n" + deskripsiminuman + "\n" + detailminuman + "\n" + fotominuman + "\n";
     }
 }
